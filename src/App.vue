e7523ffd9322d4d3d8126366edfde696cb92872c
Daniele-Simonetti <daniele.simonetti99@outlook.it>
2022-01-13 21:25:25 +0100

    general structure footer

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
     <Main />
     <EndMain />
     <Footer />
+    <EndFooter />
     <!-- <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" /> -->
   </div>
@@ -15,6 +16,8 @@ import Header from "./components/Header.vue"
 import Main from "./components/Main.vue"
 import EndMain from "./components/EndMain.vue"
 import Footer from "./components/Footer.vue"
+import EndFooter from "./components/EndFooter.vue"
+
 
 export default {
   name: "App",
@@ -24,6 +27,7 @@ export default {
     Main,
     EndMain,
     Footer,
+    EndFooter,
   },
 };
 </script>
---
+++ src/components/EndFooter.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="endfooter">
+    <div class="container">
+      <button>Sign-up now!</button>
+      <ul>
+        <li>
+          <a href="">Follow Us</a>
+        </li>
+        <li>
+          <img src="../assets/img/footer-facebook.png" alt="facebook">
+        </li>
+        <li>
+          <img src="../assets/img/footer-twitter.png" alt="twitter">
+        </li>
+        <li>
+          <img src="../assets/img/footer-youtube.png" alt="youtube">
+        </li>
+        <li>
+          <img src="../assets/img/footer-pinterest.png" alt="pinterest">
+        </li>
+        <li>
+          <img src="../assets/img/footer-periscope.png" alt="periscope">
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EndFooter",
+  data() {
+
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+  @import "../assets/scss/partials/_variables.scss";
+
+  .endfooter {
+    background-color: $endFooterColor;
+    
+    .container {
+      display: flex;
+      justify-content: space-between;
+      padding: 1em 0;
+      ul {
+        list-style: none;
+        display: flex;
+        align-items: center;
+        li {
+          padding: 0 0.5em;
+          a {
+            width: 16%;
+            text-decoration: none;
+            color: $wardBlueColor;
+            text-transform: uppercase;
+            font-weight: bold;
+          }
+        }
+      }
+    }
+  }
+</style>
